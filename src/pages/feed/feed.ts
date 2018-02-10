e711a034fed810b4f74a32060938d0b3f9e5654c
DiegoCarvalho <diego.sistemas.carvalho@gmail.com>
2018-02-10 21:24:21 -0200

    new variable

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -14,6 +14,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'feed.html',
 })
 export class FeedPage {
+  public nomeUsuario: string  = "Diego Adenir Código";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
